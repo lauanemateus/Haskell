584f23414a75872bc8d006457d493d83dbfd762b
lauanemateus <88512406+lauanemateus@users.noreply.github.com>
2024-12-03 09:59:49 -0300

    Update and rename Stack.hs to Stack.hs

--- Exercises/Atividade8_Tipos_Abstratos/Stack.hs
+++ Exercises/Atividade8_Tipos_Abstratos/Stack.hs
@@ -1,8 +1,8 @@
 module Stack (Stack, -- exportar o tipo
 push, pop, top, -- exportar as operac¸oes ˜
-empty, isEmpty, size) where
+empty_stack, isEmpty, size) where
 
-data Stack a = Stk [a] -- implementac¸ao usando listas ˜
+data Stack a = Stk [a] -- implementação usando listas 
     deriving(Show)
 
 push :: a -> Stack a -> Stack a
@@ -16,8 +16,8 @@ top :: Stack a -> a
 top (Stk (x:_)) = x
 top _ = error "Stack.top: empty stack"
 
-empty :: Stack a
-empty = Stk []
+empty_stack :: Stack a
+empty_stack = Stk []
 
 isEmpty :: Stack a -> Bool
 isEmpty (Stk [])= True
