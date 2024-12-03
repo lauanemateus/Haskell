2dbb9538316b4cd73af2ff07ccd4c55e49a495fe
lauanemateus <88512406+lauanemateus@users.noreply.github.com>
2024-12-03 09:58:56 -0300

    Update and rename Set.hs to Set.hs

---
+++ Exercises/Atividade8_Tipos_Abstratos/Set.hs
@@ -0,0 +1,49 @@
+module Set (Set, insert, empty_set, member, union, intersect, difference) where
+
+data Set a = No a (Set a) (Set a)
+            | Vazia
+            deriving (Eq,Show)
+            
+empty_set :: Set a
+empty_set = Vazia
+
+insert :: Ord a => a -> Set a -> Set a
+insert x Vazia = No x Vazia Vazia
+insert x (No y esq dir) 
+    | x==y = No y esq dir -- já ocorre
+    | x<y = No y (insert x esq) dir -- insere à esquerda
+    | x>y = No y esq (insert x dir ) -- insere à direita
+
+member :: Ord a => a -> Set a -> Bool
+member x Vazia = False -- não ocorre
+member x (No y esq dir)
+    | x==y = True -- encontrou
+    | x<y = member x esq -- procura à esquerda
+    | x>y = member x dir -- procura à direita
+
+union :: Ord a => Set a -> Set a -> Set a
+union conj1 Vazia = conj1
+union Vazia conj2 = conj2
+union conj1 (No x esq dir) = union (insert x conj1) (union esq dir)
+
+intersect :: Ord a => Set a -> Set a -> Set a
+intersect conj1 conj2 = intersectAcc conj1 conj2 empty_set
+    where 
+        intersectAcc conj1 Vazia resposta = resposta
+        intersectAcc Vazia conj2 resposta = resposta
+        intersectAcc conj1 (No x esq dir) resposta 
+            | member x conj1 = intersectAcc conj1 (union esq dir) (insert x resposta)
+            | otherwise = intersectAcc conj1 (union esq dir) resposta
+
+difference :: Ord a => Set a -> Set a -> Set a
+difference conj1 conj2 = differenceAcc conj1 conj2 empty_set
+    where 
+        differenceAcc conj1 Vazia resposta = union resposta conj1
+        differenceAcc Vazia conj2 resposta = resposta
+        differenceAcc (No x esq dir) conj2 resposta 
+            | member x conj2 = differenceAcc (union esq dir) conj2 resposta
+            | otherwise = differenceAcc (union esq dir) conj2 (insert x resposta)
+
+-- exemplo
+conjunto1 = No 4 (No 2 (No 1 Vazia Vazia) (No 3 Vazia Vazia)) (No 6 (No 5 Vazia Vazia) (No 7 Vazia Vazia))
+conjunto2 = No 5 (No 3 (No 6 Vazia Vazia) (No 8 Vazia Vazia)) (No 11 (No 12 Vazia Vazia) (No 13 Vazia Vazia))
